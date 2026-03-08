fd89d5849f8fb6953e4a66fd698341859e91ed48
davidsebas348-hub <davidsebas348@gmail.com>
2026-03-08 03:26:51 -0500

    Update Text95.lua

--- Text95.lua
+++ Text95.lua
@@ -1,26 +1,63 @@
--- Force WalkSpeed 20
--- LOCAL SCRIPT
+--------------------------------------------------
+-- TOGGLE
+--------------------------------------------------
+if getgenv().FORCE_SPEED then
+	getgenv().FORCE_SPEED = false
+	
+	if getgenv().FORCE_SPEED_CONN then
+		getgenv().FORCE_SPEED_CONN:Disconnect()
+		getgenv().FORCE_SPEED_CONN = nil
+	end
+	
+	return
+end
 
+getgenv().FORCE_SPEED = true
+
+--------------------------------------------------
+-- SERVICIOS
+--------------------------------------------------
 local player = game.Players.LocalPlayer
 local RUN = game:GetService("RunService")
 
+--------------------------------------------------
+-- FORZAR VELOCIDAD
+--------------------------------------------------
 local function forceSpeed(humanoid)
-    RUN.Heartbeat:Connect(function()
-        if humanoid and humanoid.Parent then
-            if humanoid.WalkSpeed ~= 20 then
-                humanoid.WalkSpeed = 20
-            end
-        end
-    end)
+
+	if getgenv().FORCE_SPEED_CONN then
+		getgenv().FORCE_SPEED_CONN:Disconnect()
+	end
+
+	getgenv().FORCE_SPEED_CONN = RUN.Heartbeat:Connect(function()
+
+		if not getgenv().FORCE_SPEED then return end
+
+		if humanoid and humanoid.Parent then
+			if humanoid.WalkSpeed ~= 24 then
+				humanoid.WalkSpeed = 24
+			end
+		end
+
+	end)
+
 end
 
--- Cuando aparece el personaje
+--------------------------------------------------
+-- RESPAWN
+--------------------------------------------------
 player.CharacterAdded:Connect(function(char)
-    local humanoid = char:WaitForChild("Humanoid")
-    forceSpeed(humanoid)
+
+	if not getgenv().FORCE_SPEED then return end
+
+	local humanoid = char:WaitForChild("Humanoid")
+	forceSpeed(humanoid)
+
 end)
 
--- Si el personaje ya estaba cargado
+--------------------------------------------------
+-- SI YA ESTÁ CARGADO
+--------------------------------------------------
 if player.Character and player.Character:FindFirstChild("Humanoid") then
-    forceSpeed(player.Character.Humanoid)
+	forceSpeed(player.Character.Humanoid)
 end
